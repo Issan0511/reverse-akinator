68a3abf87537cfa928fd26d25e1e8eb2fdaeda3e
Issa Nakatsuka <issanakatsuka0511@gmail.com>
2025-03-23 11:55:35 +0900

    タイトル変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '逆ネーター',
+  title: '逆ネーター--立場逆転アキネーター',
   description: 'はい・いいえの質問でキャラクターを当てる逆アキネーター的ゲーム！',
   generator: 'v0.dev',
 }
